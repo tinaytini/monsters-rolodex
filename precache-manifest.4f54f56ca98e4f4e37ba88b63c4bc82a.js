d189591b8b2a92c6e81094ef7fe9e2dd521cf38f
tinaytini <tinaytini@yahoo.com>
2020-09-13 22:42:19 +0800

    recovered

---
+++ precache-manifest.4f54f56ca98e4f4e37ba88b63c4bc82a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4d2a8ceed73c14f97933782d1cef62f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c245563642696b35816e",
+    "url": "/monsters-rolodex/static/css/main.9168c701.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c245563642696b35816e",
+    "url": "/monsters-rolodex/static/js/main.43abc477.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
